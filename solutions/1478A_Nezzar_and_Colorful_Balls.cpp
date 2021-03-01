402dfdb6bb0922776cb73116de6c705db3f405cb
rheallyc <vireonxi@gmail.com>
2021-03-01 21:11:41 +0000

    Solve 1478A: Nezzar and Colorful Balls

---
+++ solutions/1478A_Nezzar_and_Colorful_Balls.cpp
@@ -0,0 +1,31 @@
+// Problem: Nezzar and Colorful Balls
+// URL: https://codeforces.com/problemset/problem/1478/A
+// Rating: 800
+// Tags: brute force, greedy
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, a[110];
+
+void solve(){
+    scanf("%d", &n);
+    for(int i=1; i<=n; i++) a[i]=0;
+    for(int i=1; i<=n; i++){
+        int num;
+        scanf("%d", &num);
+        a[num]++;
+    }
+    int maxi=0;
+    for(int i=1; i<=n; i++){
+        maxi=max(maxi, a[i]);
+    }
+    printf("%d\n", maxi);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
