95b1e58431e6018d56e326ea566b88e3af88dd41
rheallyc <vireonxi@gmail.com>
2021-09-23 03:30:29 +0000

    Solve 1491A: K-th Largest Value

---
+++ solutions/1491A_K-th_Largest_Value.cpp
@@ -0,0 +1,31 @@
+// Problem: K-th Largest Value
+// URL: https://codeforces.com/problemset/problem/1491/A
+// Rating: 800
+// Tags: brute force, greedy, implementation
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  LL;
+int n, q, a[110000], cnt1;
+
+int main(){
+    scanf("%d%d", &n, &q);
+    cnt1=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &a[i]);
+        if(a[i]) cnt1++;
+    }
+    int t, k;
+    while(q--){
+        scanf("%d%d", &t, &k);
+        if(t==1){
+            if(a[k]==1) cnt1--;
+            else cnt1++;
+            a[k]=1-a[k];
+        }
+        else{
+            if(k<=cnt1) printf("1\n");
+            else printf("0\n");
+        }
+    }
+}
