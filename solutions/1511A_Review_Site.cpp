4343488046a330fd46e97dd64383074fc5b07698
rheallyc <vireonxi@gmail.com>
2023-02-04 23:19:06 +0000

    Solve 1511A: Review Site

---
+++ solutions/1511A_Review_Site.cpp
@@ -0,0 +1,26 @@
+// Problem: Review Site
+// URL: https://codeforces.com/problemset/problem/1511/A
+// Rating: 800
+// Tags: greedy
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+
+void solve(){
+    scanf("%d" ,&n);
+    int num, cnt=0;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &num);
+        if(num!=2) cnt++;
+    }
+    printf("%d\n", cnt);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
