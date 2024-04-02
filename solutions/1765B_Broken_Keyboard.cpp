8f9179aad5d2282a92b76addee4d8d8cc97fb8db
rheallyc <vireonxi@gmail.com>
2024-04-02 00:06:20 +0000

    Solve 1765B: Broken Keyboard

---
+++ solutions/1765B_Broken_Keyboard.cpp
@@ -0,0 +1,60 @@
+// Problem: Broken Keyboard
+// URL: https://codeforces.com/problemset/problem/1765/B
+// Rating: 800
+// Tags: greedy
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = (n) - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<int, LL> PIL;
+typedef pair<double, double> PDD;
+typedef pair<ull, ull> PUU;
+typedef pair<LL, LL> PLL;
+
+const int N = 110000;
+const int M = 1100000;
+const int mod = 1e9+7;
+const int inf = (int)1e9;
+const LL INF = 1e18;
+const double eps = 1e-9;
+
+int T, n;
+string s; 
+int main() {
+	IO;
+	cin >> T;
+	while (T--) {
+		cin >> n;
+		cin >> s;
+		if (n % 3 == 2) {
+			cout << "NO\n";
+			continue;
+		}
+		int flag = 0;
+		for (int i = 1; i < n; i += 3) {
+			if (s[i] != s[i + 1]) flag = 1;
+		}
+		if (flag) cout << "NO\n";
+		else cout << "YES\n";
+	}
+	return 0;
+}
