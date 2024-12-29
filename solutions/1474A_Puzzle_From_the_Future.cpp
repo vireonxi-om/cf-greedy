ac336dc852d8d64042b0b2ba6da77f42de0fd176
rheallyc <vireonxi@gmail.com>
2024-12-29 03:07:50 +0000

    Solve 1474A: Puzzle From the Future

---
+++ solutions/1474A_Puzzle_From_the_Future.cpp
@@ -0,0 +1,29 @@
+// Problem: Puzzle From the Future
+// URL: https://codeforces.com/problemset/problem/1474/A
+// Rating: 800
+// Tags: greedy
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+char a[110000], b[110000];
+
+void solve(){
+    scanf("%d", &n);
+    scanf("%s", &b);
+    a[0]='1';
+    for(int i=1; i<n; i++){
+        if(b[i]+'1'==a[i-1]+b[i-1]) a[i]='0';
+        else a[i]='1';
+    }
+    for(int i=0; i<n; i++) printf("%c", a[i]);
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
