7c1f90ad3125c0a24a3e5e3b5789a1875c30a729
rheallyc <vireonxi@gmail.com>
2025-09-01 02:40:44 +0000

    Solve 1486A: Shifting Stacks

---
+++ solutions/1486A_Shifting_Stacks.cpp
@@ -0,0 +1,35 @@
+// Problem: Shifting Stacks
+// URL: https://codeforces.com/problemset/problem/1486/A
+// Rating: 900
+// Tags: greedy, implementation
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n;
+ll sum;
+
+void solve(){
+    scanf("%d", &n);
+    ll num;
+    sum=0;
+    bool can=true;
+    for(int i=1; i<=n; i++){
+        scanf("%lld", &num);
+        sum+=num;
+        if(2*sum<(ll)i*(i-1)) can=false;
+    }
+    if(can) printf("YES\n");
+    else printf("NO\n");
+}
+
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
+
